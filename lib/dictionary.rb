76b706680162d107c0026f1345114226df54cec8
Wylie Mickelson <wyliecoyote910@gmail.com>
2022-06-10 09:22:17 -0700

    created a class to load a word dictionary based on a length range

---
+++ lib/dictionary.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+
+class Dictionary
+  def initialize(shortest = 5, longest = 12)
+    @words = retrieve_default_words.select { |word| word.length.between?(shortest, longest) }
+    p @words
+  end
+end
+
+def retrieve_default_words
+  fname = "../word_dict.txt"
+  unless File.file?(fname)
+    server_file = URI.open("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt")
+    File.open("../word_dict.txt", "w") { |file| file.write(server_file.read) }
+  end
+  File.open(fname, "r") { |file| file.readlines(chomp: true) }
+end
+
+Dictionary.new
