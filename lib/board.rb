167c070e64f1d75fe02ea45560c55162d2b2cfcc
Wylie Mickelson <wyliecoyote910@gmail.com>
2022-06-10 09:51:57 -0700

    created basic board layout and display

---
+++ lib/board.rb
@@ -0,0 +1,28 @@
+require_relative 'dictionary.rb'
+
+class Board
+  attr_reader :goal_word
+  attr_accessor :remaining_guesses, :remaining_letters, :correct_letters
+
+  def initialize
+    @goal_word = Dictionary.new.words.sample
+    @remaining_guesses = 6
+    @remaining_letters = ('a'..'z').to_a
+    @correct_letters = []
+
+    #testing
+    # @goal_word = "hello"
+    # @correct_letters = ["h", "l", "o"]
+  end
+
+  def display_goal_word
+    goal_word.chars.map { |char| correct_letters.include?(char) ? char : "_" }.join(" ")
+  end
+
+  def display
+    puts "Available letters: #{remaining_letters.join(" ")}\n\n"
+    puts "To guess: #{display_goal_word}"
+  end
+end
+
+Board.new.display
