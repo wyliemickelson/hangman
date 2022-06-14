4a96996512071d800fab7876f4fe51ef033fdf3c
Wylie Mickelson <wyliecoyote910@gmail.com>
2022-06-14 09:51:21 -0700

    modified turn inputs

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,19 @@
 require_relative 'board.rb'
+require_relative 'validation.rb'
 
 class Game
-  attr_reader :board
+  include Validation
+  attr_reader :board, :current_action
 
   def initialize
     @board = Board.new
+    @current_action = nil
+  end
+
+  def set_current_action(input)
+    action = input == '1' ? :save : :guess
+    @current_action = action
+    puts @current_action.to_s
   end
 
   def game_over?
@@ -24,18 +33,14 @@ class Game
     puts ">> The word was: #{board.goal_word}"
   end
 
-  def valid_char(char)
-    board.remaining_letters.include?(char)
-  end
-
-  def get_guess
-    puts ">> Enter your letter guess: "
-    guess = gets.chomp.downcase
-    until valid_char(guess)
+  def get_turn_input
+    puts ">> Enter your letter guess or '1' to save: "
+    input = gets.chomp.downcase
+    until valid_turn_input(input)
       puts "Invalid input."
-      guess = gets.chomp.downcase
+      input = gets.chomp.downcase
     end
-    guess
+    input
   end
 
   def correct_guess?(char)
@@ -43,14 +48,20 @@ class Game
   end
 
   def turn
-    player_input = get_guess
-    board.remove_letter(player_input)
-    if correct_guess?(player_input)
-      board.add_correct_letter(player_input)
-    else
-      board.remaining_guesses -= 1
+    player_input = get_turn_input
+    set_current_action(player_input)
+    if current_action == :save
+      #save the game
+    elsif current_action == :guess
+      board.remove_letter(player_input)
+      if correct_guess?(player_input)
+        board.add_correct_letter(player_input)
+      else
+        board.remaining_guesses -= 1
+      end
+      board.display
     end
-    board.display
+  
   end
 end
 
